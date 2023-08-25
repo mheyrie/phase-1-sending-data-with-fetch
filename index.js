9d45b473df1efbadbf1c6afb89ca706d1e366a9a
Mary <omota4fidelity@gmail.com>
2023-08-25 13:03:14 +0100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        name: userName,
+        email: userEmail,
+      }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      document.body.append(data.id);
+    })
+    .catch(error => {
+      document.body.append(error.message);
+    });
+  }
